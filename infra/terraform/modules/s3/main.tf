a74f1a0afe9747755ed895efbb2860cdcca2530c
Eduard Usatchev <52632342+EduardUsatchev@users.noreply.github.com>
2025-07-16 17:29:54 +0300

    Update main.tf

--- infra/terraform/modules/s3/main.tf
+++ infra/terraform/modules/s3/main.tf
@@ -1,7 +1,27 @@
 resource "aws_s3_bucket" "this" {
   bucket = var.bucket_name
 
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
   tags = {
     Name = "shopnserve-bucket"
   }
+}
+
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
+  block_public_acls   = true
+  block_public_policy = true
+  ignore_public_acls  = true
+  restrict_public_buckets = true
 }
