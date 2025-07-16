9efc81fa278c93afbf7db87c8ccd6167dc03e798
Eduard Usatchev <52632342+EduardUsatchev@users.noreply.github.com>
2025-07-16 17:25:20 +0300

    Update variables.tf

--- infra/terraform/modules/ec2/variables.tf
+++ infra/terraform/modules/ec2/variables.tf
@@ -1,3 +1,18 @@
+variable "ami_id" {
+  description = "AMI ID for EC2 instances"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.small"
+  validation {
+    condition     = contains(["t2.micro", "t3.small", "t3.medium"], var.instance_type)
+    error_message = "Must be a valid instance type."
+  }
+}
+
 variable "vpc_id" {
   description = "The VPC ID where instances are launched"
   type        = string
