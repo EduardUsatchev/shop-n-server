a829b6a7bfa6f0ed6ce017fc4ce38c14a41f78fa
Eduard Usatchev <52632342+EduardUsatchev@users.noreply.github.com>
2025-07-16 17:31:33 +0300

    Update main.tf

--- infra/terraform/modules/sqs/main.tf
+++ infra/terraform/modules/sqs/main.tf
@@ -1,3 +1,19 @@
 resource "aws_sqs_queue" "this" {
-  name = var.queue_name
+  name                       = var.queue_name
+  visibility_timeout_seconds = var.visibility_timeout
+  message_retention_seconds  = var.retention_seconds
+
+  # Optionally add encryption, DLQ, etc.
+}
+
+variable "visibility_timeout" {
+  description = "Visibility timeout for the SQS queue"
+  type        = number
+  default     = 30
+}
+
+variable "retention_seconds" {
+  description = "Message retention in seconds"
+  type        = number
+  default     = 345600  # 4 days
 }
